962d732a5a2f562ad8e4866d9610ff35dce08366
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-01 23:53:09 +0530

    a21

--- src/bin/a21.rs
+++ src/bin/a21.rs
@@ -25,4 +25,15 @@ fn find_user(name: &str) -> Option<i32> {
     }
 }
 
-fn main() {}
+fn main() {
+    let username = "sam";
+    let user = find_user(username).map(|user_id| User {
+        user_id,
+        name: username.to_owned(),
+    });
+
+    match user {
+        Some(user) => println!("{:?}", user),
+        None => println!("No user found"),
+    };
+}
