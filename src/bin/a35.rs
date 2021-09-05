a93f5bf33aa792f60086e4ef089bebbd548439ac
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-05 11:54:31 +0530

    a35

--- src/bin/a35.rs
+++ src/bin/a35.rs
@@ -15,7 +15,7 @@
 // * Grass, Dirt, and Sand should all print "Ground tile"
 // * Treasure Chests:
 //   * If the treasure is Gold and the amount is at least 100, print "Lots of gold!"
-// * Everything else shoud not print any messages
+// * Everything else should not print any messages
 //
 // Notes:
 // * Use a single match expression utilizing guards to implement the program
@@ -54,4 +54,34 @@ enum Tile {
     Wood,
 }
 
-fn main() {}
+fn print_tile(tile: Tile) {
+    use Tile::*;
+    match tile {
+        Brick(brick @ BrickStyle::Gray | brick @ BrickStyle::Red) => {
+            println!("Brick color is {:?}", brick);
+        },
+        Brick(other) => println!("{:?} brick", other),
+        Dirt | Grass | Sand => println!("Ground tile"),
+        Treasure(TreasureChest { amount, .. }) if amount >=  100 => println!("Lots of gold"),
+        Water(pressure) if pressure.0 < 10 => println!("Water pressure level: {:?}", pressure),
+        Water(pressure) if pressure.0 >= 10 => println!("High pressure"),
+        _ => (),
+    }
+}
+
+fn main() {
+    let tile = Tile::Brick(BrickStyle::Red);
+    print_tile(tile);
+
+    let tile = Tile::Sand;
+    print_tile(tile);
+
+    let tile = Tile::Treasure(TreasureChest{
+        content: TreasureItem::Gold,
+        amount: 200,
+    });
+    print_tile(tile);
+
+    let tile = Tile::Water(Pressure(9));
+    print_tile(tile);
+}
