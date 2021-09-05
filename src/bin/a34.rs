21ab0c25fba84ba780929ab04c5327b1ffe4a522
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-05 10:40:57 +0530

    a34

--- src/bin/a34.rs
+++ src/bin/a34.rs
@@ -16,4 +16,54 @@
 // Notes:
 // * Optionally use generics for each state
 
-fn main() {}
+#[derive(Copy, Clone)]
+struct LuggageId(usize);
+
+struct Luggage(LuggageId);
+
+struct CheckIn(LuggageId);
+struct OnLoad(LuggageId);
+struct OffLoad(LuggageId);
+struct AwaitingPickup(LuggageId);
+struct EndCustody(LuggageId);
+
+impl Luggage {
+    fn new(id: LuggageId) -> Self {
+        Luggage(id)
+    }
+
+    fn check_in(self) -> CheckIn {
+        CheckIn(self.0)
+    }
+}
+
+impl CheckIn {
+    fn onload(self) -> OnLoad {
+        OnLoad(self.0)
+    }
+}
+
+impl OnLoad {
+    fn offload(self) -> OffLoad {
+        OffLoad(self.0)
+    }
+}
+
+impl OffLoad {
+    fn carousel(self) -> AwaitingPickup {
+        AwaitingPickup(self.0)
+    }
+}
+
+impl AwaitingPickup {
+    fn pickup(self) -> (Luggage,EndCustody) {
+        (Luggage(self.0) ,EndCustody(self.0))
+    }
+}
+
+fn main() {
+    let id = LuggageId(1);
+    let luggage = Luggage::new(id);
+    let luggage = luggage.check_in().onload().offload().carousel();
+    let (luggage, _) = luggage.pickup();
+}
