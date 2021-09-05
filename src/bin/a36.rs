2eb6cb4759be1cae3d51fae661bc1f2bbad9e989
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-05 12:16:49 +0530

    a36

--- src/bin/a36.rs
+++ src/bin/a36.rs
@@ -14,7 +14,20 @@ fn data() -> &'static [u64] {
     &[5, 5, 4, 4, 3, 3, 1]
 }
 
+fn process_chunk(data: &[u64]) {
+    match data {
+        [lhs, rhs] => println!("{} + {} = {}", lhs, rhs, (lhs + rhs)),
+        [single] => println!("Unpaired values: {}", single),
+        [] => println!("Data stream complete"),
+        [..] => unreachable!("chunk size should be at most 2")
+    }
+}
+
 fn main() {
     // `stream` is an iterator of Option<&[u64]>
     let mut stream = data().chunks(2);
+
+    for chunk in stream {
+        process_chunk(chunk);
+    }
 }
